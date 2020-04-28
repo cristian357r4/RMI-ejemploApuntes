3cde083166509af0c73d79a4f2aec56fbebfed8b
Cristian Estrada Peña <cristian357r4@gmail.com>
2020-04-28 11:38:32 -0500

    se agrego el nuevo bind de Hola, se pueden agregar varios al registro

--- src/RMIClient/OperacionClienteRMI.java
+++ src/RMIClient/OperacionClienteRMI.java
@@ -1,5 +1,6 @@
 package RMIClient;
 
+import RMIInterface.Hola;
 import RMIInterface.OperacionInterfaz;
 
 import java.rmi.registry.LocateRegistry;
@@ -8,12 +9,15 @@ import java.rmi.registry.Registry;
 public class OperacionClienteRMI {
   public static void main(String []args){
     OperacionInterfaz servidorObj;
+    Hola mensaje;
     String serverAdress = "192.168.0.15";
     int serverPort = 3232;
     try{
       Registry registry = LocateRegistry.getRegistry(serverAdress,serverPort);
       servidorObj= (OperacionInterfaz)(registry.lookup("operacionservidor"));
+      mensaje = (Hola)(registry.lookup("mensaje"));
       System.out.println("Mayor: "+servidorObj.calcularMayor(100,500));
+      System.out.println(mensaje.hola("Cristian"));
 
     }catch(Exception e){
       e.printStackTrace();
